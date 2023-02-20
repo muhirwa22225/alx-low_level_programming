08a03057e028e700287baaaab48a50d8fa7641d1
Your Name <you@example.com>
2023-02-20 03:28:31 -0800

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * main - a program that prints exactly the string provided
+ * Return: Always 0
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");  
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line using printf function 
- * Return: Always 0 (Success)
-*/
+ * main - A program that prints
+ * a line using a printf function
+ * Return: 0 (Always success)
+ */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
