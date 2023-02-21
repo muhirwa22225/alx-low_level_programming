e95a8228dc6018e481da37db6016d7fb9791b201
Your Name <you@example.com>
2023-02-21 06:53:24 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - A program that prints a putchar
+ * followed by a new line
+ * Return: 0 (Always Success)
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+In this file, we are going to learn all about the loops in C programming
