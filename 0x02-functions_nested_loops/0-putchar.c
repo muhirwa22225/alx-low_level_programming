7c59cfa10e38b9de57ec8cbd22ba8bec348e374e
Your Name <you@example.com>
2023-02-21 08:16:01 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,14 @@
 
 int main(void)
 {
-	char word[9] = "_putchar";
-
-	int i = 0;
-
-
-	while (i < 8)
-	{
-		_putchar(word[i]);
-		i++;
-	}
-	_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
+
