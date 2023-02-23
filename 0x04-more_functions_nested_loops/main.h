c1323b23393ce8d78ffb9d0a1d30961b6e9649ba
Your Name <you@example.com>
2023-02-23 09:00:24 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Justin Muhirwa
+ * Desc: Header contains protypes for all functions
+ */
+int _putchar(char c);
+
+
+
+
+
+
+#endif
