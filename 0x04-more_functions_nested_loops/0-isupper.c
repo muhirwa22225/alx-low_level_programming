63e4653b3384aeba5eff9b0c74fba01262faf3d3
Your Name <you@example.com>
2023-02-23 08:54:07 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,4 +5,14 @@
  * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _putchar(char c); 
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
