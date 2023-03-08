5be9e8163c97ecfe8aad31f8e4e954ba17cf8924
muhirwa22225 <muhirwajustin@gmail.com>
2023-03-08 12:06:56 -0800

    6-prime.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+able File  16 lines (14 sloc)  229 Bytes
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to string
+ * *Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
