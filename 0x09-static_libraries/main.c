4f2b11ec42f643f8e5800dc397b07718b5748b92
muhirwa22225 <muhirwajustin@gmail.com>
2023-03-10 05:25:28 -0800

    changed empty

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	puts("\"Enjoy the moment, because one day you will wish to enjoy it while you don't have it.\"\n- Justin Muhirwa");
+	return 0;
+}
