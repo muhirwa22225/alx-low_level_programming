42eb304bd785fe4a0668f65b4d03800470178cf7
muhirwa22225 <muhirwajustin@gmail.com>
2023-03-10 10:51:32 -0800

    amen

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# Compile all the .c files in the current directory
 gcc -c *.c
-
-# Create the static library liball.a
 ar rcs liball.a *.o
-
-# Clean up the object files
 rm *.o
-
-echo "Static library liball.a created successfully!"
