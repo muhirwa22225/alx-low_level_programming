b24b8587ece16906d5c738a837267f349c35cf31
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-04-04 13:37:02 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Return: Number of Nodes
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+In this project, I am going to work on the more function of the single linked lists
