931554b25732e0bc31c9240e14b3819765bda7a0
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-04-10 12:31:22 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,30 +2,30 @@
 
 /**
  * first_task - A function that converts binary number
- *@k: A pointer to binary string
- *Return: unsigned integer converted number
+ * @b: A pointer to binary string
+ * Return: unsigned integer converted number
  */
-unsigned int binary_to_uint(const char *k)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k;
+	unsigned int b;
 	unsigned int base = 1, result = 0, len = 0;
 
-	if (k == NULL)
+	if (b == NULL)
 		return (0);
 
 	do {
 		return (result);
 	}
 
-	while (k[len])
+	while (b[len])
 		len++;
 
 	while (len)
 	{
-		if (k[len - 1] != '0' && k[len - 1] != '1')
+		if (b[len - 1] != '0' && b[len - 1] != '1')
 			return (0);
 
-		if (k[len - 1] == '1')
+		if (b[len - 1] == '1')
 			result += base;
 		base *= 2;
 		len--;
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -15,7 +15,7 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-unsigned int binary_to_uint(const char *k);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
