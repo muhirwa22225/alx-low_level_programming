3b2c386fd5599c7ceae8cbb08e0ff520399c6097
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-04-14 13:48:57 -0700

    This is the file i/o functions that have been used

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * append_text_to_file - A function that appends text at the end of a file
+ * @text_content: The NULL terminated string to add
+ * @filename: The filename to open and append in
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fdo, fdw, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdo = open(filename, O_RDWR | O_APPEND);
+	if (fdo < 0)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fdo);
+		return (1);
+	}
+
+	while (*(text_content + len))
+		len++;
+
+	fdw = write(fdo, text_content, len);
+	close(fdo);
+	if (fdw < 0)
+		return (-1);
+	return (1);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This directory will tell us all about the file descriptors that are used in c
