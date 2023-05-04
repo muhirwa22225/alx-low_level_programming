71fcd3a5bea10783bf79ebf8dbb67ebd6c7ff795
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-05-04 10:39:11 -0700

    unsigned int binary_to_uint(const char *b) { unsigned int res = 0; int base = 1, i = 0;
    if (b == NULL)
    return (0);
    
    while (b[i + 1])
    {
    if (b[i] != '0' && b[i] != '1')
    return (0);
    i++;
    }
    
    while (i >= 0)
    {
    res += ((b[i] - '0') * base);
    base *= 2;
    i--;
    }
    
    return (res);
    
    }

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,41 +6,29 @@
  * @b: This point to the string
  * Return: the converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base = 1, ret = 0, len = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
 	if (b == NULL)
-	{
 		return (0);
-	}
 
-	while (b[len])
+	while (b[i + 1])
 	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-
-		if (b[len - 1] == '1')
-			ret += base;
-		base *= 2;
-		len--;
+		i++;
 	}
-	return (ret);
-}
 
-/**
- * main - Entry point of the function
- * Return: 0 Always (success)
- */
-int main(void)
-{
-	binary_to_uint("1");
-	binary_to_uint("101");
-	binary_to_uint("1e01");
-	binary_to_uint("1100010");
-	binary_to_uint("0000000000000000000110010010");
-	return (0);
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - A file containing all the prototypes of the function
  */
