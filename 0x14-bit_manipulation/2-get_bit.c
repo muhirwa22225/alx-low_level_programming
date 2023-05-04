589f860f0cc1fee767593ca1c0a95ee703a67d27
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-05-04 12:19:27 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * get_bit - A function to return the bit of the integer
+ * @n: integer to be evaluated on
+ * @index: the starting form of the index
+ * Return: index value or -1 if error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int max = 0x01;
+
+	max <<= index;
+	if (max == 0)
+		return (-1);
+
+	if ((n & max))
+		return (1);
+	else
+		return (0);
+}
