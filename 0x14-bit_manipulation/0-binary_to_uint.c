88c3a08474715dc96793e68d4d5f9c0bad6f5bd3
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-05-04 10:47:35 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,27 +8,27 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int base = 1, i = 0;
+	unsigned int ret = 0;
+	int base = 1, j = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i + 1])
+	while (b[j + 1])
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[j] != '0' && b[j] != '1')
 			return (0);
-		i++;
+		j++;
 	}
 
-	while (i >= 0)
+	while (j >= 0)
 	{
-		res += ((b[i] - '0') * base);
+		ret += ((b[j] - '0') * base);
 		base *= 2;
-		i--;
+		j--;
 	}
 
 
-	return (res);
+	return (ret);
 
 }
