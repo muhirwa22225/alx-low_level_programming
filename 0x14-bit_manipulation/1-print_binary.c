0db5841cd4c4b3fdaad5f79532ec5a046ecd53f3
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-05-04 12:06:15 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,31 +2,13 @@
 
 /**
  * print_binary - A function that prints the binary representation of a number
- * main - Entry point of a function
  * @n: integer to be printed
  * Return: 0 Always (success)
  */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
 
-void print_binary(unsigned long int n) {
-    int size = sizeof(unsigned long int) * 8;
-    unsigned long int mask = (unsigned long int)1 << (size - 1);
-    
-    while (mask > 0) {
-        if (n & mask) {
-            putchar('1');
-        } else {
-            putchar('0');
-        }
-        mask >>= 1;
-    }
+	putchar((n & 1) + '0');
 }
-
-int main() {
-    unsigned long int n = 123456789;
-    printf("Binary representation of %lu: ", n);
-
-    print_binary(n);
-    putchar('\n');
-    return 0;
-}
-
