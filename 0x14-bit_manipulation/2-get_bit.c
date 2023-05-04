c18d49197e923a66b9c8e91fe627684662b8d1b0
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-05-04 12:42:40 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,11 +12,18 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int max = 0x01;
 
 	max <<= index;
-	if (max == 0)
-		return (-1);
 
-	if ((n & max))
+	if (max == 0)
+	{
+		return (-1);
+	}
+	else if ((n & max))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+	return (index);
 }
