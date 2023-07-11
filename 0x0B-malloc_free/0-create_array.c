dc50491b91a88d5fe441a15699ea8f27a51396e6
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-07-11 05:09:42 -0700

    my first program

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - This function creates an array of other function
+ * @size: size of the unsigned integer
+ * @c: character
+ *
+ * Return: Returns to the array.
+**/
+
+char *create_array(unsigned int size, char c)
+{
+    char *array = NULL;
+    unsigned int i;
+    
+    if(size == 0)
+    return (NULL);
+    if (size != 0)
+    {
+        array = (char *)malloc(size * sizeof(char));
+        if (array != NULL)
+        {
+            for (i = 0; i < size; i++)
+            array[i] = c;
+        }
+    }
+    return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is my first memory allocation program where I would be working with memory free allocation on the heap of the arrays. 
