62fb12ba325b82b64b75051be8939c034dd384ce
muhirwa22225 <muhirwajustin2003@gmail.com>
2023-07-13 09:42:45 -0700

    This is the start of the project and I hopeI am advancing in technology

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - memory allocation
+ * @b: number of bytes to be allocated
+ * Return: a pointer or an address to be allocated
+ */
+
+void *malloc_checked(unsigned int b)
+{
+        void *ptr;
+
+        ptr = malloc(b);
+
+        if (ptr == NULL)
+                exit(98);
+
+        return (ptr);
+}
